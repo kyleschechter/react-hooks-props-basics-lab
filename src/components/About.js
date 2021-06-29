75dbbf03d6e320ff5164952a4f0c31e90614e806
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-06-29 15:38:51 -0400

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const bio = props.bio
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && bio.length > 1 ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links 
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
